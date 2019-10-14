ade08f96c51af6fa9d48a293c66b60ed526a5eee
user <hiroaking717@gmail.com>
2019-10-14 15:23:45 +0900

    [update]sharedファイルの一時的な破棄

--- IdeasBank/src/app/shared/language/language.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class LanguageService {
-  code = window.navigator.language.substr(0, 2) === 'ja' ? 'ja' : 'en';
-}
